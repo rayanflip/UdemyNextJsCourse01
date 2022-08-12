420aef1f5b9238cffc11702e950d0c3b73f8f0ba
rayanflip <109683081+rayanflip@users.noreply.github.com>
2022-08-12 21:44:12 +0400

    load items from db using useEffect and useState to avoid infinite loop and map json object to an array using '...' op Very important comments on avoiding infinite loop with useEffect and why we need useState to avoid js execution while maintaining the sync state of react component

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 const DUMMY_DATA = [
   {
@@ -21,6 +22,48 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage(props) {
+  //this function returns a promise, and js does not wait for it to complete. We cannot use await/async as this will
+  //not qualitfy as react component (react components are synchronous). Hence we need to change the screen to loading spinner
+  // and once the loading finishes we change it back, this can be achieved using useState.
+  // BEWARE of infinite loops, as everytime set functions are called it will re-execute the component function so it should be handled in the code using useEffect.
+  const [isLoading, setIsLoading] = useState(true); //set it to true initially as we expect that when this component is loading data is being fetched and loaded.
+  const [loadedData, setLoadedData] = useState([]); // another useState to set the data.
+  // useEffect to handle infinite loop scenario of useState.
+  // The first argument is a function that will be executed by react on our behalf undre certain circumstances.
+  // the second argument is the condition under which react will execute the function.
+  // If not provided at all, it same as if executes everytime hence no need to use useEffect.
+  // if provided but empty [], that means react will run it once.
+  // if provided, react will check the values and compare them to the same variables of the last run, if it changed then it runs the function
+  // as a rule of thumb, the arguments should be external values that require re-execution of the function.
+  useEffect(() => {
+    setIsLoading(true); //weird
+    fetch(
+      "https://reactjs-tutorial-cca75-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key] // copies the key value pairs of the data[key] object into the meetup object
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false); //we got the data here, set is loading to false which in turn tells react to change the state and reload.
+        setLoadedData(meetups); //be careful here of possible infinite loop as this will also instruct react to change state and reload (triggering the previous useState as well)
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <div>
       <h1>All Meetups</h1>
@@ -31,7 +74,7 @@ function AllMeetupsPage(props) {
           return <li key={meetup.id}>{meetup.title}</li>;
         })}
       </ul>*/}
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedData} />
     </div>
   );
 }
